45919cb8430a95a85456f51d1ae8ef34f27ac955
joel06-dsouza <joeld9946@gmail.com>
2023-09-25 17:44:21 +0530

    Updating

--- dvd-rental/dvd-spring-server/src/main/java/in/mindcraft/controllers/JacksonConfig.java
+++ dvd-rental/dvd-spring-server/src/main/java/in/mindcraft/controllers/JacksonConfig.java
@@ -1,7 +1,7 @@
 package in.mindcraft.controllers;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-// import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
