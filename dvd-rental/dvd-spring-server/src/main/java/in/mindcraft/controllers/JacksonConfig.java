e41036fc3910c8ce1ba92190fb0fc30c76074732
joel06-dsouza <joeld9946@gmail.com>
2023-09-25 14:16:23 +0530

    Server Added

---
+++ dvd-rental/dvd-spring-server/src/main/java/in/mindcraft/controllers/JacksonConfig.java
@@ -0,0 +1,18 @@
+package in.mindcraft.controllers;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+// import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class JacksonConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+        return objectMapper;
+    }
+}
