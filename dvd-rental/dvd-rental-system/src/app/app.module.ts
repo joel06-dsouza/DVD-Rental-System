a6528dc7a2b2611534efe88c084346bb461e62d7
MukulRajk <mukul.gupta@mindcraft.in>
2023-09-27 16:39:45 +0530

    Changes in Table / table component

--- dvd-rental/dvd-rental-system/src/app/app.module.ts
+++ dvd-rental/dvd-rental-system/src/app/app.module.ts
@@ -1,16 +1,21 @@
+/* app.module.ts */
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    /* LoginComponent */
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,LoginComponent,ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
