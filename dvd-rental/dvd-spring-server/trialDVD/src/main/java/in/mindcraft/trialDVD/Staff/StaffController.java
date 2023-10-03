35ff816ff65c02e7f0ace772e7a49d9fd0b94e1e
satvikagrasani <satvikagrasani@gmail.com>
2023-10-03 20:22:39 +0530

    JWT Commit

--- dvd-rental/dvd-spring-server/trialDVD/src/main/java/in/mindcraft/trialDVD/Staff/StaffController.java
+++ dvd-rental/dvd-spring-server/trialDVD/src/main/java/in/mindcraft/trialDVD/Staff/StaffController.java
@@ -4,15 +4,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+
 import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/staff")
+@RequestMapping("/Staff")
 public class StaffController {
 	public long S_id;
     @Autowired
     private StaffRepository staffRepository;
+
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider; // Inject the JWT token provider
 
     @GetMapping("/allStaff")
     public List<Staff> getAllStaff() {
@@ -33,25 +37,25 @@ public class StaffController {
 //
 //  
     
-    @GetMapping("/login")
-    public ResponseEntity<Long> login(@RequestParam String username, @RequestParam String password) {
-        // Find all Staff with the given username
-        List<Staff> staffList = staffRepository.findAllByUsername(username);
-        String msg = "login success";
+    // @GetMapping("/login")
+    // public ResponseEntity<Long> login(@RequestParam String username, @RequestParam String password) {
+    //     // Find all Staff with the given username
+    //     List<Staff> staffList = staffRepository.findAllByUsername(username);
+    //     String msg = "login success";
 
-        for (Staff staff : staffList) {
-            if (staff.getPassword().equals(password)) {
-                long S_id = staff.getStoreId(); // Assuming getStoreId() is the getter method for storeId
-                System.out.println("Store ID "+S_id);
-                return ResponseEntity.ok(S_id);
-            }
-        }
+    //     for (Staff staff : staffList) {
+    //         if (staff.getPassword().equals(password)) {
+    //             long S_id = staff.getStoreId(); // Assuming getStoreId() is the getter method for storeId
+    //             System.out.println("Store ID "+S_id);
+    //             return ResponseEntity.ok(S_id);
+    //         }
+    //     }
 
 
 
-        // If no matching user was found, return an error response
-        return ResponseEntity.notFound().build();
-    }
+    //     // If no matching user was found, return an error response
+    //     return ResponseEntity.notFound().build();
+    // }
     
 
 //    @GetMapping("/allFilmInfo")
@@ -64,6 +68,33 @@ public class StaffController {
 //            return ResponseEntity.notFound().build();
 //        }
 //    }
+
+    @PostMapping("/login")
+    public ResponseEntity<JwtToken> login(@RequestParam String username, @RequestParam String password) {
+        // Find all Staff with the given username
+        List<Staff> staffList = staffRepository.findAllByUsername(username);
+
+        for (Staff staff : staffList) {
+            if (staff.getPassword().equals(password)) {
+                long storeId = staff.getStoreId();
+
+                // Generate a JWT token including additional claims
+                String token = jwtTokenProvider.generateToken(username, storeId);
+
+                // Create a JwtToken object to hold the token and additional information
+                JwtToken jwtToken = new JwtToken();
+                jwtToken.setJwtToken(token);
+                // jwtToken.setAccountType("Saving Account"); // Add additional information
+
+                // Return the JwtToken object as a response
+                return ResponseEntity.ok(jwtToken);
+            }
+        }
+
+        // If no matching user was found, return an error response
+        return ResponseEntity.notFound().build();
+    }
+
 
 }
 
--- dvd-rental/dvd-spring-server/trialDVD/src/main/java/in/mindcraft/trialDVD/TrialDvdApplication.java
+++ dvd-rental/dvd-spring-server/trialDVD/src/main/java/in/mindcraft/trialDVD/TrialDvdApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.core.JdbcTemplate;
+
+
 import javax.sql.DataSource;
 //@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 @SpringBootApplication
