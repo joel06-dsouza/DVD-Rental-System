d3b2267c1dc80ac8d229c9ce444bc2c877fdc521
satvikagrasani <satvikagrasani@gmail.com>
2023-10-20 17:36:01 +0530

    Working Basic Jwt Security

--- dvd-rental/dvd-spring-server/src/main/java/in/mindcraft/server/JwtUtil.java
+++ dvd-rental/dvd-spring-server/src/main/java/in/mindcraft/server/JwtUtil.java
@@ -5,7 +5,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
+// import org.springframework.stereotype.Component;
 
 import in.mindcraft.server.pojos.UserInfo;
 
